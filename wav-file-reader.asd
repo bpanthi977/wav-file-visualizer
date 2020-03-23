f2c2871a3d820735bf5c4a9a6d0626d421422b1b
Bibek Panthi <bpanthi977@gmail.com>
2020-03-23 16:38:47 +0545

    reads single stream data too

--- wav-file-reader.asd
+++ wav-file-reader.asd
@@ -6,6 +6,6 @@
   :license  "Specify license here"
   :version "0.0.1"
   :serial t
-  :depends-on (#:com.gigamonkeys.binary-data)
+  :depends-on (#:com.gigamonkeys.binary-data #:harmony-simple)
   :components ((:file "package")
                (:file "wav-file-reader")))
--- wav-file-reader.lisp
+++ wav-file-reader.lisp
@@ -20,16 +20,43 @@
 (define-binary-type ub3 () (unsigned-integer-bigendian :bytes 3 :bits-per-byte 8))
 (define-binary-type ub4 () (unsigned-integer-bigendian :bytes 4 :bits-per-byte 8))
 
+(define-binary-type unsigned-bytes (bytes)
+  (:reader (in)
+		   (loop with array = (make-array bytes :element-type '(unsigned-byte 8))
+				 for i from 0 to (1- bytes) do
+				   (setf (aref array i) (read-byte in))
+				 finally (return array)))
+  (:writer (out value)
+		   (loop for byte across value do
+				 (write-byte byte out))))
+
+(define-binary-type signed-2bytes (size)
+  (:reader (in)
+		   (loop with array = (make-array size :element-type '(signed-byte 16))
+				 for i from 0 to (1- size)
+				 for byte1 = (read-byte in)
+				 for byte2 = (read-byte in)
+				 for int = 0 do
+				   (setf (ldb (byte 8 8) int) byte1
+						 (ldb (byte 8 0) int) byte2)
+				   (if (= (ldb (byte 1 7) byte1) 1)
+				   	   (setf int (- int (expt 2 16))))
+				   (setf (aref array i) int)
+				 finally (return array)))
+  (:writer (out value)
+		   (loop for byte across value do
+			 (write-byte byte out))))
 
 (define-tagged-binary-class
 	wav  ()
 	((riff-identifier (iso-8859-1-string :length 4))
-	 (size (ub4))
+	 (size (ub4)) ;; file-size - 4
 	 (wave-identifier (iso-8859-1-string :length 4)))
 	(:dispatch (if (and (string-equal riff-identifier "RIFF")
 						(string-equal wave-identifier "WAVE"))
 				   'wav-1
 				   (error "File is not a WAVE file"))))
+
 
 
 
@@ -44,10 +71,11 @@
 	 (bytes-per-sample*channels (ub2))
 	 (bits-per-sample (ub2))
 	 (data-chunk-identifier (iso-8859-1-string :length 4))
-	 (file-size (ub4))))
-					 
+	 (data-size (ub4)) ;;file-size - 44
+	 (data (signed-2bytes :size (/ data-size 2)))))
 
 (defun tt()
   (with-open-file (stream *file* :element-type 'unsigned-byte)
-	(read-value 'wav stream )))
+	 (read-value 'wav stream)))
+
 	
